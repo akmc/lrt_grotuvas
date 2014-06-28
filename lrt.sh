1c4a5178b24adbd78ba5a50ea4fdbbe567c19c52
akmc <liudas@akmc.lt>
2014-06-28 09:41:11 +0300

    Pridėtos radijo stotys. diff geriau rodo kas dar

--- lrt.sh
+++ lrt.sh
@@ -17,39 +17,62 @@
 #===================================================================================
 
 #-----------------------------------------------------------------------------------
-# Kintamieji
+# Nustatymai
 #-----------------------------------------------------------------------------------
 
 grotuvas='mpv'							# video grotuvas
-lrt='http://www.lrt.lt/mediateka/tiesiogiai'		# URL iki kanalo
+lrt='http://www.lrt.lt/mediateka/tiesiogiai'			# URL iki kanalo
 
-# grotuvas_param='--vf crop=1050:574:0:2 --deinterlace=yes'	# papildomi video grotuvo parametrai
+# grotuvas_param='--vf crop=1050:574:0:2 --deinterlace=yes'	# instrukcijos PASISKAITOME.md faile
 
 #-----------------------------------------------------------------------------------
 # Pagrindinė scenarijaus dalis
 #-----------------------------------------------------------------------------------
-# Patikrina ar egzistuoja pasirinktas grotuvas. Jeigu ne tai praneša vartotojui apie tai ir baigia darbą kodu 1
+
+# Patikrinama ar yra įdiegtas nurodytas grotuvas. Jei neįdiegtas - nutraukiamas scenarijus ir pranešama vartotojui
 command -v ${grotuvas} >/dev/null 2>&1 || { echo "Nerastas grotuvas! Patikrinkite konfiguraciją ${0} failo viršuje."; exit 1; }
+# Patikrinama ar yra įdiegtas curl. Jei neįdiegtas - nutraukiamas scenarijus ir pranešama vartotojui
+command -v curl >/dev/null 2>&1 || { echo "Nerastas grotuvas! Patikrinkite konfiguraciją ${0} failo viršuje."; exit 1; }
  
 case ${1} in
     -h|--help)
         echo "Naudojimas: ${0} [kanalas]"
         echo ""
         echo "TV kanalai:"
-        echo " tv1 (1) - Lietuvos televizija"
-        echo " tv2 (2) - LRT Kultūra"
-        echo " tv3 (3) - LRT Kultūra"
+        echo " 1 | tv1 | televizija = LRT Televizija"
+        echo " 2 | tv2 | kultura = LRT Kultūra"
+        echo " 3 | tv3 | lituanica = LRT Lituanika"
+        echo ""
         echo "Radijo kanalai:"
+        echo " 4 | r1 | radijas = LRT Radijas"
+        echo " 5 | r2 | klasika = LRT Klasika"
+        echo " 6 | r3 | opus = LRT Klasika"
+        echo ""
+        echo "LRT televizijai rodyti skirtų komandų pavyzdžiai:"
+        echo "    Komanda: \"sh ${0} 1\""
+        echo " arba"
+        echo "    Komanda: \"sh ${0} tv1\""
+        echo " arba"
+        echo "    Komanda: \"sh ${0} televizija\""
         echo ""
         ;;
-    tv1|1)
-        nohup ${grotuvas} ${grotuvas_param} $(curl -s ${lrt}/lrt-televizija | grep -oP 'rtmp\S+[a-z0-9]{32}')/ltv1 &
+    1|tv1|televizija)
+        nohup ${grotuvas} ${grotuvas_param} $(curl -s ${lrt}/lrt-televizija | grep -oP 'rtmp\S+[a-z0-9]{32}')/LTV1 &
         ;;
-    tv2|2)
+    2|tv2|kultura)
         nohup ${grotuvas} ${grotuvas_param} $(curl -s ${lrt}/lrt-kultura | grep -oP 'rtmp\S+[a-z0-9]{32}')/ltv2 &
         ;;
-    tv3|3)
+    3|tv3|lituanica)
         nohup ${grotuvas} ${grotuvas_param} $(curl -s ${lrt}/lrt-lituanica | grep -oP 'rtmp\S+[a-z0-9]{32}')/world &
+        ;;
+    4|r1|radijas)
+        ${grotuvas} ${grotuvas_param} $(curl -s ${lrt}/lrt-radijas | grep -oP 'rtmp\S+[a-z0-9]{32}')/radio.mp3
+        ;;
+    5|r2|klasika)
+        ${grotuvas} ${grotuvas_param} $(curl -s ${lrt}/lrt-klasika | grep -oP 'rtmp\S+[a-z0-9]{32}')/radio.mp3
+        ;;
+    6|r3|opus)
+        ${grotuvas} ${grotuvas_param} $(curl -s ${lrt}/lrt-opus | grep -oP 'rtmp\S+[a-z0-9]{32}')/radio.mp3
         ;;
     *)
         echo "Nenurodytas kanalas!"
@@ -62,5 +85,6 @@ esac
 #-----------------------------------------------------------------------------------
 # TODO
 #-----------------------------------------------------------------------------------
-# 1. Pasitikrinimą ar transliuojama (kartais netransliuojama internetu dėl autorinių teisių)
-# 2. Papildyti radijo kanalais
+# 1. Pasitikrinimą ar transliuojama (kartais netransliuojama internetu dėl autorinių teisių. galima gaudyt tekstą html'e, arba grep statusą "null")
+# 2. Langas radijo transliacijai (kad išsijungti būtų galima žmoniškai :))
+# 3. Mintis - gal būtų šaunu rodyti statinį radijo kanalo paveiksliuką audio grojimo metu (sumixuoti skirtingus video ir audio kanalaus)
